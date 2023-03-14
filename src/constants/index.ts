569e3831304ead5c1d6309088371ed08b5c667fd
Mlbb Fan <77885074+retjehnetwork@users.noreply.github.com>
2023-03-15 04:40:41 +0700

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -54,7 +54,7 @@ export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
 export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
   [ChainId.MAINNET]: [
     [
-      new Token(ChainId.MAINNET, '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82', 18, 'CAKE', 'PancakeSwap Token'),
+      new Token(ChainId.MAINNET, '0x1a639e150d2210a4be4a5f0857a9151b241e7ae4', 18, 'BOW TOKEN', 'PancakeSwap Token'),
       new Token(ChainId.MAINNET, '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c', 18, 'WBNB', 'Wrapped BNB')
     ],
     [BUSD, USDT],
